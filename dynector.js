77b21e91defa23f8f0e389ae5d0a568471babf3d
ashleybrener <ashley@starlogik.com>
2013-03-17 21:35:18 +0200

    fine tuning

---
+++ dynector.js
@@ -0,0 +1,233 @@
+﻿var events = require('events');
+var https = require('https');
+var util = require('util');
+var winston = require('winston');
+
+var logger = new (winston.Logger)({
+	transports: [
+		new (winston.transports.Console)({
+			colorize: true,
+			handleExceptions: true,
+			timestamp: true
+		})
+	]
+});
+
+winston.addColors({
+	info: 'green',
+	debug: 'yellow',
+	warn: 'purple',
+	error: 'red'
+});
+
+var Session = require('./session');
+
+function Dynector(customer, username, password, keepalive) {
+	events.EventEmitter.call(this);
+
+	this.connected = false;
+	this.session = new Session(customer, username, password);
+	this.timer_id = '';
+
+	var self = this;
+
+	this.session.on('opened', function (token) {
+		self.connected = true;
+		self.emit('connected', token);
+
+		if (keepalive) {
+			self.timer_id = setInterval(function () {
+				self.session.keepAlive();
+			}, keepalive);
+		}
+
+		winston.info('[Dynector] connected', { token: token, keepAlive: keepalive });
+	});
+
+	function onClosed() {
+		self.connected = false;
+		self.emit('disconnected');
+
+		if (keepalive && self.timer_id != '') {
+			clearInterval(self.timer_id);
+			self.timer_id = '';
+		}
+
+		winston.info('[Dynector] disconnected');
+	}
+
+	this.session.on('closed', onClosed);
+
+	this.session.on('error', function () {
+		if (self.connected) {
+			onClosed();
+			self.session.open();
+		}
+	});
+}
+
+util.inherits(Dynector, events.EventEmitter);
+module.exports = Dynector;
+
+Dynector.prototype.connect = function () {
+	this.session.open();
+}
+
+Dynector.prototype.disconnect = function () {
+	this.session.close();
+}
+
+Dynector.prototype.publishZone = function (zone, callback) {
+	this.session.execute('PUT', '/Zone/' + zone + '/', {
+		data: { publish: 'true' }
+	}, function (response) {
+		//{'status': 'success', 
+		//'data': {'zone_type': 'Primary', 'serial_style': 'day', 'serial': 2013010101, 'zone': 'example.com'},  
+		//'job_id': 8969227,  
+		//'msgs': [{'INFO': 'publish: example.com published', 'SOURCE': 'BLL', 'ERR_CD': null, 'LVL': 'INFO'}]}
+
+		if (callback) {
+			callback(response);
+		}
+	});
+}
+
+Dynector.prototype.getGSLBRegionPoolEntry = function (zone, fqdn, regionCode, address, callback) {
+	this.gslbRegionPoolEntry('GET', zone, fqdn, regionCode, address, {}, callback);
+}
+
+Dynector.prototype.addGSLBRegionPoolEntry = function(zone, fqdn, regionCode, address, label, weight, serveMode, callback) {
+	this.gslbRegionPoolEntry('POST', zone, fqdn, regionCode, address, {
+		rdata: {
+			address: address,
+			label: label,
+			weight: weight,
+			serve_mode: serveMode
+		}
+	}, callback);
+
+	//{'status': 'success', 
+	//'data': { 
+	//'zone': 'example.com',  
+	//'ttl': 60,  
+	//'fqdn': 'www.example.com',  
+	//'record_type': 'A',  
+	//'rdata': {'address': '12.13.14.15'},  
+	//'record_id': 0 
+	//},  
+	//'job_id': 8965933,  
+	//'msgs': [{'INFO': 'add: Record added', 'SOURCE': 'BLL', 'ERR_CD': null, 'LVL': 'INFO'}]}
+}
+
+Dynector.prototype.editGSLBRegionPoolEntry = function (zone, fqdn, regionCode, address, newAddress, label, weight, serveMode, callback) {
+	this.gslbRegionPoolEntry('PUT', zone, fqdn, regionCode, address, {
+		rdata: {
+			new_address: newAddress,
+			label: label,
+			weight: weight,
+			serve_mode: serveMode
+		}
+	}, callback);
+}
+
+Dynector.prototype.removeGSLBRegionPoolEntry = function (zone, fqdn, regionCode, address, callback) {
+	this.gslbRegionPoolEntry('DELETE', zone, fqdn, regionCode, address, {}, callback);
+}
+
+Dynector.prototype.gslbRegionPoolEntry = function (method, zone, fqdn, regionCode, address, data, callback)
+{
+	var path = '/GSLBRegionPoolEntry/' + zone + '/' + fqdn + '/' + regionCode + '/';
+	var options = {};
+
+	if (method !== 'POST') {
+		path += address + '/';
+	}
+
+	if (method === 'POST' || method === 'PUT') {
+		options.data = data;
+	}
+
+	this.session.execute(method, path, options, function (response) {
+		if (callback) {
+			callback(response);
+		}
+	});
+}
+
+Dynector.prototype.getRecordSet = function (type, zone, fqdn, callback) {
+	this.record(type, 'GET', zone, fqdn, null, {}, callback);
+
+	//{'status': 'success', 
+	//'data': ['/REST/ARecord/example.com/www.example.com/13179845'], 
+	//'job_id': 8301511, 
+	//'msgs': [{'INFO': 'detail: Found 1 record', 'SOURCE': 'BLL', 'ERR_CD': null, 'LVL': 'INFO'}]}
+}
+
+Dynector.prototype.getRecord = function (type, zone, fqdn, recordId, callback) {
+	this.record(type, 'GET', zone, fqdn, recordId, {}, callback);
+
+	//{'status': 'success', 
+	//'data': {
+	//'zone': 'example.com', 
+	//'ttl': 30, 
+	//'fqdn': 'www.example.com', 
+	//'record_type': 'A', 
+	//'rdata': {'address': '12.13.14.15'}, 
+	//'record_id': 13179845 
+	//}, 
+	//'job_id': 8307490, 
+	//'msgs': [{'INFO': 'get: Found the record', 'SOURCE': 'API-B', 'ERR_CD': null, 'LVL': 'INFO'}]}
+}
+
+Dynector.prototype.addRecord = function (type, zone, fqdn, rdata, ttl, callback) {
+	this.record(type, 'POST', zone, fqdn, null, {
+		rdata: rdata,
+		ttl: ttl
+	}, callback);
+
+	//{'status': 'success', 
+	//'data': { 
+	//'zone': 'example.com',  
+	//'ttl': 60,  
+	//'fqdn': 'www.example.com',  
+	//'record_type': 'A',  
+	//'rdata': {'address': '12.13.14.15'},  
+	//'record_id': 0 
+	//},  
+	//'job_id': 8965933,  
+	//'msgs': [{'INFO': 'add: Record added', 'SOURCE': 'BLL', 'ERR_CD': null, 'LVL': 'INFO'}]}
+}
+
+Dynector.prototype.editRecords = function (type, zone, fqdn, records, callback) {
+	this.record(type, 'PUT', zone, fqdn, recordId, records, callback);
+}
+
+Dynector.prototype.editRecord = function (type, zone, fqdn, recordId, rdata, ttl, callback) {
+	this.record(type, 'PUT', zone, fqdn, recordId, {
+		rdata: rdata,
+		ttl: ttl
+	}, callback);
+}
+
+Dynector.prototype.removeRecord = function (type, zone, fqdn, recordId, callback) {
+	this.record(type, 'DELETE', zone, fqdn, recordId, {}, callback);
+}
+
+Dynector.prototype.record = function (type, method, zone, fqdn, recordId, data, callback) {
+	var path = '/' + type + 'Record/' + zone + '/' + fqdn + '/';
+	var options = {};
+
+	if (recordId !== null) {
+		path += recordId + '/';
+	}
+
+	if (method === 'POST' || method === 'PUT') {
+		options.data = data;
+	}
+
+	this.session.execute(method, path, options, function (response) {
+		if (callback) {
+			callback(response);
+		}
+	});
+}
---
+++ session.js
@@ -0,0 +1,258 @@
+﻿var events = require('events');
+var https = require('https');
+var util = require('util');
+var winston = require('winston');
+
+var logger = new (winston.Logger)({
+	transports: [
+		new (winston.transports.Console)({
+			colorize: true,
+			handleExceptions: true,
+			timestamp: true
+		})
+	]
+});
+
+winston.addColors({
+	info: 'green',
+	debug: 'yellow',
+	warn: 'purple',
+	error: 'red'
+});
+
+function Session(customer, username, password) {
+	events.EventEmitter.call(this);
+	this.openTime = null;
+	this.customer = customer;
+	this.username = username;
+	this.password = password;
+	this.token = null;
+}
+
+util.inherits(Session, events.EventEmitter);
+module.exports = Session;
+
+Session.prototype.open = function (callback) {
+	var self = this;
+
+	send(null, 'POST', '/Session/', {
+		data: {
+			customer_name: this.customer,
+			user_name: this.username,
+			password: this.password
+		}
+	}, function (response) {
+		//{'status': 'success',
+		//'data': {
+		//'token': 'L555zUgaQaEUKPaWls9jO9A6foxWkXrNRuaFREksrqS1GsKBFwqSUrA7RKZ82/NtthnhXoSijher/Y2tyWleEDIDbdkFv/1uQUq7Vm4LJ12QPow68S3fSQJOmkv4mxuo9syvt3BOh0cOQTY2YUmNug==', 
+		//'version': '3.0.0'
+		//},
+		//'job_id': 8236528,
+		//'msgs': [{'INFO': 'login: Login successful', 'SOURCE': 'BLL', 'ERR_CD': null, 'LVL': 'INFO'}]}
+
+		var err;
+
+		if (response.status == 'success') {
+			self.token = response.data.token;
+			self.openTime = new Date();
+			self.emit('opened', self.token);
+		}
+		else {
+			err = response.msgs[0].ERR_CD;
+			self.emit('error', err);
+		}
+
+		if (callback) {
+			callback(err, self.token);
+		}
+	});
+}
+
+Session.prototype.close = function () {
+	var self = this;
+
+	send(this.token, 'DELETE', '/Session/', {}, function (response) {
+		//{ status: 'success',
+		//	data: {},
+		//	job_id: 187784305,
+		//	msgs:
+		//	[ { INFO: 'logout: Logout successful',
+		//		SOURCE: 'BLL',
+		//		ERR_CD: null,
+		//		LVL: 'INFO' } ] }
+
+		if (response.status == 'success') {
+			self.token = null;
+			self.openTime = null;
+			self.emit('closed');
+		}
+		else {
+			self.emit('error', response.msgs[0].ERR_CD);
+		}
+	});
+}
+
+Session.prototype.verify = function () {
+	var self = this;
+
+	send(this.token, 'GET', '/Session/', {}, function (response) {
+		//{ status: 'success',
+		//	data: {},
+		//	job_id: 187782563,
+		//	msgs:
+		//	[ { INFO: 'isalive: User session is still active',
+		//		SOURCE: 'BLL',
+		//		ERR_CD: null,
+		//		LVL: 'INFO' } ] }
+
+		var err;
+
+		if (response.status == 'success') {
+			self.emit('verified');
+		}
+		else {
+			self.emit('error', response.msgs[0].ERR_CD);
+		}
+
+		if (callback) {
+			callback(err);
+		}
+	});
+}
+
+Session.prototype.keepAlive = function () {
+	var self = this;
+
+	send(this.token, 'PUT', '/Session/', {}, function (response) {
+		//{ status: 'success',
+		//	data: {},
+		//	job_id: 187784261,
+		//	msgs:
+		//	[ { INFO: 'keepalive: User session extended',
+		//		SOURCE: 'BLL',
+		//		ERR_CD: null,
+		//		LVL: 'INFO' } ] }
+
+		if (response.status == 'success') {
+			self.emit('keepalive');
+		}
+		else {
+			self.emit('error', response.msgs[0].ERR_CD);
+		}
+	});
+}
+
+Session.prototype.execute = function (method, path, options, callback) {
+	var self = this;
+
+	if (this.token != null) {
+		var now = new Date();
+
+		if (now - this.openTime >= (50 * 60 * 1000)) {
+			this.verify(function (err) {
+				if (err) {
+					self.open(function (err) {
+						if (!err) {
+							send(self.token, method, path, options, callback);
+						}
+					});
+				}
+			});
+		}
+		else {
+			send(this.token, method, path, options, callback);
+		}
+	}
+	else {
+		this.open(function (err) {
+			if (!err) {
+				send(self.token, method, path, options, callback);
+			}
+		});
+	}
+}
+
+function send(token, method, path, options, callback) {
+	var opt = {
+		host: 'api2.dynect.net',
+		port: 443,
+		path: '/REST' + path,
+		method: method,
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	};
+
+	if (options.data) {
+		options.data = JSON.stringify(options.data);
+		opt.headers['Content-Length'] = options.data.length;
+	}
+	else {
+		opt.headers['Content-Length'] = 0;
+	}
+
+	if (path !== '/Session/' || method !== 'POST') {
+		if (token == null) {
+			throw new Error('must open a session first');
+		}
+
+		opt.headers['Auth-Token'] = token
+	}
+
+	var req = https.request(opt, function (res) {
+		var data = '';
+
+		res.on('readable', function () {
+			var chunk = res.read();
+			data += chunk.toString('ascii');
+		});
+
+		res.on('end', function () {
+			try {
+				var response;
+
+				try {
+					response = JSON.parse(data);
+				}
+				catch (e) {
+					response = data;
+				}
+
+				if (response.msgs) {
+					for (var i = 0; i < response.msgs.length; i++) {
+						var msg = response.msgs[i];
+
+						if (msg.ERR_CD != null) {
+							winston.error(msg.ERR_CD + ' (' + msg.INFO + ')\n');
+						}
+						else {
+							winston.info(msg.INFO + '\n');
+						}
+					}
+				}
+				else {
+					winston.info(response + '\n');
+				}
+
+				if (callback) {
+					callback(response);
+				}
+			}
+			catch (e) {
+				winston.error('[request exception] ' + e.message + '\n\n' + data);
+			}
+		});
+	});
+
+	req.on('error', function (e) {
+		winston.error('[request] ' + e);
+	});
+
+	winston.info(method + ' https://' + opt.host + opt.path);
+
+	if ((method === 'POST' || method === 'PUT') && options.data) {
+		req.write(options.data);
+	}
+
+	req.end();
+}
